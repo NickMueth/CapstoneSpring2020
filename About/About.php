547184b1b2371dd43c4322b6d7fce9eeb1d9fb0f
OASanchez <osvsanchez1@gmail.com>
2020-04-17 14:52:24 -0500

    About Changes
    Updating some parts of the about page

---
+++ About/About.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <link rel="stylesheet" type="text/css" href="About_CSS/About.css">
+    <?php
+    include('../Master/header.php');
+    ?>
+</head>
+
+<body>
+    <!-- Wrapping everything in the paw print bg, but then also including background for content-->
+    <h1> About Us </h1>
+    <div class="grid-container">
+        <div class="Osvaldo">
+            <h2> Osvaldo Sanchez </h2>
+            <img class="cover-photo" src="About_Images/Ozzy_Cover.jpg" alt="Osvaldo Sanchez Cover Photo">
+            <p><br>Hello, my name is Osvaldo Sanchez, but most call me Ozzy.  I'm a senior at Southeast Missouri State University and graduating this Spring with a Bachelors Degree in Computer Information Systems.
+            I wish to start as a Junior Developer working with OOP languages, like Java, work my way to Senior Developer, and ultimately work as a Project Manager.
+            My contributions for this website were the initial designs of all pages, the creation of the Clicked Disease Pages and this page you're on right now!
+            I also largely took on the role of organizing the team and working as the team leader.  We sincerely thank you for looking at our project and hope you will reach out to us.
+            <br> My GitHub link: https://github.com/OASanchez?tab=repositories</p>
+        </div>
+        <div class="Nick">
+            <h2> Nick Mueth </h2>
+            <img class="cover-photo" src="About_Images/Paw_Bg.png" alt="Nicholas Mueth Cover Photo">
+            <p><br>Hello, my name is Nick Mueth. I'm a student at Southeast Missouri State University graduating with a Bachelors degree in Computer Information Systems with a focus on Web Programming. 
+            After I finish school, I am getting hired by a local family-owned convenience store chain called MotoMart, to fill a brand new position in their IT Department. 
+            In our Capstone project I worked mainly on the design of the site, and the backend functionality. 
+            This project was a fun one to work on, I learned a lot about dog health and writing PHP. I hope you enjoy the site and get good use out of it.</p>
+        <div class="Josh">
+            <h2> Joshua Toth </h2>
+            <img class="cover-photo" src="About_Images/Josh_Cover.jpg" alt="Joshua Toth Cover Photo">
+            <p><br>Hello my name is Joshua Toth. I am Student at Southeast Missouri State University graduating with a Bachelors degree in Computer Information Systems and Business Administration. 
+            What I want to do in my field is work in business analysis type positions to better serve business on what their data could do for them, or do explain to them what it could possibly mean. 
+            During this project of our final semester here in College I worked mainly with the Database as well as helping to plan out the early stages of the project. 
+            Working with the data architecture is something that really set into motion what I have learned from my time here at Southeast.</p>
+        </div>
+        <div class="Parfait">
+            <h2> Parfait Domagni </h2>
+            <img class="cover-photo" src="About_Images/Paw_Bg_Blue.png" alt="Parfait Domagni Cover Photo">
+            <p><br>Short description about who the person is and what they did on this page </p>
+        </div>
+    </div>
+    <div class="sources">
+        <h1> Information Sources </h1>
+        <p> www.link.com/information </p>
+        <p> www.link.com/information </p>
+        <p> www.link.com/information </p>
+        <h1> Legal Disclaimer </h1>
+        <h1 class="Disclaimer_Txt"> WE ARE NOT DOCTORS [INSERT DISCLAIMER WHEN DRAFTED BY LEGAL PROFESSIONAL (IN PROGRESS)]</h1>
+    </div>
+
+
+</body>
+<?php
+include('../Master/footer.php');
+?>
+
+</html>
